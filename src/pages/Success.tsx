0999b5080060ac8a8ff978beb61bfa5eee6f3613
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-11 00:53:57 +0800

    Clear localCart after checkout success

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+
 export default function Success() {
+	useEffect(() => {
+		// Clear the cart
+		localStorage.setItem("cart", JSON.stringify([]));
+	}, []);
+
 	return <h1>Thank you for your purchase!</h1>;
 }
