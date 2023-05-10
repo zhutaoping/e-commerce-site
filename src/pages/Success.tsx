5228a2f53243fb1ec03d018e8d02499b30feed17
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-11 01:03:23 +0800

    Revise firebaserc

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function Success() {
 	useEffect(() => {
 		// Clear the cart
-		localStorage.setItem("cart", JSON.stringify([]));
+		localStorage.setItem("state", JSON.stringify([]));
 	}, []);
 
 	return <h1>Thank you for your purchase!</h1>;
