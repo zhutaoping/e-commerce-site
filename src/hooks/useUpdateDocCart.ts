82048d91e7e5013ea21f11074ec90c863f51d30d
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-11 04:33:05 +0800

    Clear local and db cart after checkout succeed

--- src/hooks/useUpdateDocCart.ts
+++ src/hooks/useUpdateDocCart.ts
@@ -3,18 +3,18 @@ import { auth, db } from "../firebase/config";
 import { ProductTypes } from "../types/myTypes";
 
 const updateDocCart = async (items: ProductTypes[]) => {
-  let uid: string = "";
-  if (auth.currentUser) {
-    uid = auth.currentUser.uid;
-  }
+	let uid: string = "";
+	if (auth.currentUser) {
+		uid = auth.currentUser.uid;
+	}
 
-  const userRef = doc(db, `users/${uid}`);
-  await updateDoc(userRef, {
-    items: deleteField(),
-  });
+	const userRef = doc(db, `users/${uid}`);
+	await updateDoc(userRef, {
+		items: deleteField(),
+	});
 
-  await updateDoc(userRef, {
-    items: arrayUnion(...items),
-  });
+	await updateDoc(userRef, {
+		items: arrayUnion(...items),
+	});
 };
 export default updateDocCart;
